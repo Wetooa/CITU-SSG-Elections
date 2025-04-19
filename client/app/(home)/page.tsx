13303bfa65fa81d359c8e273a307119369265c4a
wetooa <derpykidyt@gmail.com>
2025-04-20 00:28:45 +0800

    Added partial election timeline and leaderboard for engagement

--- client/app/(home)/page.tsx
+++ client/app/(home)/page.tsx
@@ -11,13 +11,19 @@ import {
 } from "@fortawesome/free-solid-svg-icons";
 import { CountdownTimer } from "@/components/home/countdown";
 import Image from "next/image";
+import ElectionTimeline from "@/components/home/timeline";
 
 type PartyList = "United" | "Just" | "Independent";
+
 interface Candidate {
   name: string;
   votes: number;
   position: string;
   partyList: PartyList;
+}
+
+interface Leaderboard extends Candidate {
+  award: string;
 }
 
 export default function HomePage() {
@@ -43,6 +49,35 @@ export default function HomePage() {
       partyList: "United",
     },
   ];
+  const candidateOfTheDayData: Candidate = {
+    name: "Adrian Sajulga",
+    votes: 120,
+    position: "President",
+    partyList: "United",
+  };
+  const leaderboardData: Leaderboard[] = [
+    {
+      name: "Adrian Sajulga",
+      votes: 120,
+      position: "President",
+      partyList: "United",
+      award: "Most Viewed",
+    },
+    {
+      name: "Adrian Sajulga",
+      votes: 120,
+      position: "President",
+      partyList: "United",
+      award: "Most Viewed",
+    },
+  ];
+
+  // FIX: Fix later for independent party list
+  const PARTYLIST_TO_IMAGE: Record<PartyList, string> = {
+    United: "/party-list/united.jpg",
+    Just: "/party-list/just.jpg",
+    Independent: "/party-list/united.jpg",
+  };
 
   return (
     <main className="flex items-center justify-center w-full p-6">
@@ -54,7 +89,7 @@ export default function HomePage() {
           <div className="flex flex-col gap-4 flex-1">
             <section className="border rounded-lg bg-gradient-to-br from-[#EF4444] to-[#340A0A] p-4">
               <div className="flex items-center gap-2 text-lg font-medium mb-2">
-                <FontAwesomeIcon icon={faHourglass} className="fa-fw" />
+                <FontAwesomeIcon icon={faHourglass} />
                 Voting starts in
               </div>
               <CountdownTimer time={targetDate} />
@@ -62,26 +97,22 @@ export default function HomePage() {
 
             <section className="border rounded-lg p-4">
               <div className="flex items-center gap-2 text-lg font-medium mb-2">
-                <FontAwesomeIcon icon={faRectangleList} className="fa-fw" />
+                <FontAwesomeIcon
+                  icon={faRectangleList}
+                  className="text-accent"
+                />
                 Voting for the day
               </div>
 
               <div>
                 {votingForTheDayData.map((candidate, index) => {
-                  const bgImage =
-                    candidate.partyList === "United"
-                      ? "bg-[url(/party-list/united.jpg)]"
-                      : candidate.partyList === "Just"
-                        ? "bg-[url(/party-list/just.jpg)]"
-                        : "bg-neutral-800";
-
                   return (
                     <div
                       key={index}
                       className="relative overflow-hidden rounded-lg mb-2 "
                     >
                       <div
-                        className={`absolute inset-0 ${bgImage} bg-center bg-cover`}
+                        className={`absolute inset-0 bg-[url(${PARTYLIST_TO_IMAGE[candidate.partyList]})] bg-center bg-cover`}
                       />
                       <div className="absolute inset-0 bg-black opacity-40" />
 
@@ -113,7 +144,7 @@ export default function HomePage() {
 
             <section className="border rounded-lg bg-gradient-to-br from-[#EF4444] to-[#340A0A] p-4">
               <div className="flex items-center gap-2 text-lg font-medium mb-2">
-                <FontAwesomeIcon icon={faNewspaper} className="fa-fw" />
+                <FontAwesomeIcon icon={faNewspaper} />
                 Latest News
               </div>
             </section>
@@ -121,15 +152,95 @@ export default function HomePage() {
 
           {/* Right Column */}
           <div className="flex flex-col gap-4 flex-1">
-            <div className="bg-black h-40 rounded-xl p-4 text-white flex items-center justify-center">
-              Candidate of the Day
-            </div>
-            <div className="bg-neutral-800 h-40 rounded-xl p-4 text-white flex items-center justify-center">
-              Election Timeline
-            </div>
-            <div className="bg-gradient-to-br from-red-500 to-[#340A0A] h-40 rounded-xl p-4 text-white flex items-center justify-center">
-              Leaderboard for Engagement
-            </div>
+            <section className="border rounded-lg p-4">
+              <div className="flex items-center gap-2 text-lg font-medium mb-2">
+                <FontAwesomeIcon icon={faStar} className="text-accent" />
+                Candidate of the Day
+              </div>
+
+              <div className="relative overflow-hidden rounded-lg mb-2 ">
+                <div
+                  className={`absolute inset-0 bg-[url(${
+                    PARTYLIST_TO_IMAGE[candidateOfTheDayData.partyList]
+                  })] bg-center bg-cover`}
+                />
+                <div className="absolute inset-0 bg-black opacity-40" />
+
+                <div className="relative z-10 flex items-end justify-between">
+                  <div className="space-y-1 p-4">
+                    <p className="uppercase leading-16 m-0 text-7xl font-bebas">
+                      {candidateOfTheDayData.name}
+                    </p>
+                    <p className="italic leading-normal text-sm">
+                      For {candidateOfTheDayData.position}
+                    </p>
+                  </div>
+
+                  <Image
+                    src={"/candidates/duterte-placeholder.png"}
+                    alt={"SSG Logo"}
+                    width={100}
+                    height={100}
+                  />
+                </div>
+              </div>
+            </section>
+
+            <section className="border rounded-lg p-4 bg-gradient-to-tr from-[#18181B] to-[#FF6969]">
+              <div className="flex items-center gap-2 text-lg font-medium mb-2">
+                <FontAwesomeIcon
+                  icon={faCalendarDays}
+                  className="text-accent"
+                />
+                Election Timeline
+              </div>
+
+              <ElectionTimeline />
+            </section>
+
+            <section className="border rounded-lg p-4">
+              <div className="flex items-center gap-2 text-lg font-medium mb-2">
+                <FontAwesomeIcon icon={faFire} className="text-accent" />
+                Leaderboard for Engagement
+              </div>
+
+              <div className="flex">
+                {leaderboardData.map((candidate, index) => {
+                  return (
+                    <div
+                      key={index}
+                      className="relative overflow-hidden rounded-lg mb-2 "
+                    >
+                      <div
+                        className={`absolute inset-0 bg-[url(${PARTYLIST_TO_IMAGE[candidate.partyList]})] bg-center bg-cover`}
+                      />
+                      <div className="absolute inset-0 bg-black opacity-40" />
+
+                      <div className="relative z-10 flex items-end justify-between">
+                        <div className="space-y-1 p-4">
+                          <p className="rounded-lg bg-white text-accent font-xs px-2 py-1 w-fit">
+                            {candidate.award}
+                          </p>
+                          <p className="uppercase leading-normal m-0 text-4xl font-bebas">
+                            {candidate.name}
+                          </p>
+                          <p className="italic leading-normal text-sm">
+                            For {candidate.position}
+                          </p>
+                        </div>
+
+                        <Image
+                          src={"/candidates/duterte-placeholder.png"}
+                          alt={"SSG Logo"}
+                          width={100}
+                          height={100}
+                        />
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </section>
           </div>
         </div>
       </div>
