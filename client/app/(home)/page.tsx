d86e80dbc5f85b8e560eddc60d1ceb40755b581f
wetooa <derpykidyt@gmail.com>
2025-04-20 01:20:53 +0800

    Cleaned layout

--- client/app/(home)/page.tsx
+++ client/app/(home)/page.tsx
@@ -61,160 +61,150 @@ export default function HomePage() {
   ];
 
   return (
-    <main className="flex items-center justify-center w-full p-6">
-      <div className="w-full max-w-[1000px]">
-        <h1 className="text-3xl font-bold text-white mb-6">Home</h1>
+    <div>
+      <h1 className="text-3xl font-bold text-white mb-6">Home</h1>
 
-        <div className="flex gap-6 w-full">
-          {/* Left Column */}
-          <div className="flex flex-col gap-4 flex-1">
-            <section className="border rounded-lg bg-gradient-to-br from-[#EF4444] to-[#340A0A] p-4">
-              <div className="flex items-center gap-2 text-lg font-medium mb-2">
-                <FontAwesomeIcon icon={faHourglass} />
-                Voting starts in
-              </div>
-              <CountdownTimer time={targetDate} />
-            </section>
+      <div className="flex gap-6 w-full">
+        {/* Left Column */}
+        <div className="flex flex-col gap-4 flex-1">
+          <section className="border rounded-lg bg-gradient-to-br from-[#EF4444] to-[#340A0A] p-4">
+            <div className="flex items-center gap-2 text-lg font-medium mb-2">
+              <FontAwesomeIcon icon={faHourglass} />
+              Voting starts in
+            </div>
+            <CountdownTimer time={targetDate} />
+          </section>
 
-            <section className="border rounded-lg p-4">
-              <div className="flex items-center gap-2 text-lg font-medium mb-2">
-                <FontAwesomeIcon
-                  icon={faRectangleList}
-                  className="text-accent"
-                />
-                Voting for the day
-              </div>
+          <section className="border rounded-lg p-4">
+            <div className="flex items-center gap-2 text-lg font-medium mb-2">
+              <FontAwesomeIcon icon={faRectangleList} className="text-accent" />
+              Voting for the day
+            </div>
 
-              <div className="space-y-2">
-                {votingForTheDayData.map((candidate, index) => {
-                  const bgImage = PARTYLIST_TO_IMAGE[candidate.partyList];
+            <div className="space-y-2">
+              {votingForTheDayData.map((candidate, index) => {
+                const bgImage = PARTYLIST_TO_IMAGE[candidate.partyList];
 
-                  return (
-                    <ImageDiv
-                      bgImage={bgImage}
-                      key={index}
-                      className="flex items-end justify-between"
-                    >
-                      <>
-                        <div className="space-y-1 p-4">
-                          <p className="rounded-lg bg-accent px-2 py-1 w-fit">
-                            {candidate.votes} votes
-                          </p>
-                          <p className="uppercase leading-normal m-0 text-4xl font-bebas">
-                            {candidate.name}
-                          </p>
-                          <p className="italic leading-normal text-sm">
-                            For {candidate.position}
-                          </p>
-                        </div>
-
-                        <Image
-                          src={CANDIDATE_TO_IMAGE[candidate.name]}
-                          alt={"SSG Logo"}
-                          width={100}
-                          height={100}
-                        />
-                      </>
-                    </ImageDiv>
-                  );
-                })}
-              </div>
-            </section>
-
-            <section className="border rounded-lg bg-gradient-to-br from-[#EF4444] to-[#340A0A] p-4">
-              <div className="flex items-center gap-2 text-lg font-medium mb-2">
-                <FontAwesomeIcon icon={faNewspaper} />
-                Latest News
-              </div>
-            </section>
-          </div>
-
-          {/* Right Column */}
-          <div className="flex flex-col gap-4 flex-1">
-            <section className="border rounded-lg p-4">
-              <div className="flex items-center gap-2 text-lg font-medium mb-2">
-                <FontAwesomeIcon icon={faStar} className="text-accent" />
-                Candidate of the Day
-              </div>
-
-              <ImageDiv
-                bgImage={PARTYLIST_TO_IMAGE[candidateOfTheDayData.partyList]}
-                className="flex items-end justify-between"
-              >
-                <>
-                  <div className="space-y-1 p-4">
-                    <p className="uppercase leading-16 m-0 text-7xl font-bebas">
-                      {candidateOfTheDayData.name}
-                    </p>
-                    <p className="italic leading-normal text-sm">
-                      For {candidateOfTheDayData.position}
-                    </p>
-                  </div>
-
-                  <Image
-                    src={CANDIDATE_TO_IMAGE[candidateOfTheDayData.name]}
-                    alt={"SSG Logo"}
-                    width={100}
-                    height={100}
-                  />
-                </>
-              </ImageDiv>
-            </section>
-
-            <section className="border rounded-lg p-4 bg-gradient-to-tr from-[#18181B] to-[#FF6969]">
-              <div className="flex items-center gap-2 text-lg font-medium mb-2">
-                <FontAwesomeIcon
-                  icon={faCalendarDays}
-                  className="text-accent"
-                />
-                Election Timeline
-              </div>
-
-              <ElectionTimeline />
-            </section>
-
-            <section className="border rounded-lg p-4">
-              <div className="flex items-center gap-2 text-lg font-medium mb-4 text-white">
-                <FontAwesomeIcon icon={faFire} className="text-accent" />
-                Leaderboard for Engagement
-              </div>
-
-              <div className="flex gap-4 overflow-x-auto pb-2">
-                {leaderboardData.map((candidate, index) => (
+                return (
                   <ImageDiv
-                    bgImage={
-                      PARTYLIST_TO_IMAGE[candidateOfTheDayData.partyList]
-                    }
+                    bgImage={bgImage}
                     key={index}
                     className="flex items-end justify-between"
                   >
                     <>
-                      <div className="space-y-1 p-2">
-                        <p className="rounded bg-white text-accent text-xs px-2 py-1 w-fit font-semibold">
-                          {candidate.award}
+                      <div className="space-y-1 p-4">
+                        <p className="rounded-lg bg-accent px-2 py-1 w-fit">
+                          {candidate.votes} votes
                         </p>
-                        <p className="uppercase text-white text-3xl font-bebas">
+                        <p className="uppercase leading-normal m-0 text-4xl font-bebas">
                           {candidate.name}
                         </p>
-                        <p className="italic text-white text-sm">
+                        <p className="italic leading-normal text-sm">
                           For {candidate.position}
                         </p>
                       </div>
 
                       <Image
                         src={CANDIDATE_TO_IMAGE[candidate.name]}
-                        alt={`${candidate.name} image`}
-                        width={80}
-                        height={80}
+                        alt={"SSG Logo"}
+                        width={100}
+                        height={100}
                       />
                     </>
                   </ImageDiv>
-                ))}
-              </div>
-            </section>
-          </div>
+                );
+              })}
+            </div>
+          </section>
+
+          <section className="border rounded-lg bg-gradient-to-br from-[#EF4444] to-[#340A0A] p-4">
+            <div className="flex items-center gap-2 text-lg font-medium mb-2">
+              <FontAwesomeIcon icon={faNewspaper} />
+              Latest News
+            </div>
+          </section>
+        </div>
+
+        {/* Right Column */}
+        <div className="flex flex-col gap-4 flex-1">
+          <section className="border rounded-lg p-4">
+            <div className="flex items-center gap-2 text-lg font-medium mb-2">
+              <FontAwesomeIcon icon={faStar} className="text-accent" />
+              Candidate of the Day
+            </div>
+
+            <ImageDiv
+              bgImage={PARTYLIST_TO_IMAGE[candidateOfTheDayData.partyList]}
+              className="flex items-end justify-between"
+            >
+              <>
+                <div className="space-y-1 p-4">
+                  <p className="uppercase leading-16 m-0 text-7xl font-bebas">
+                    {candidateOfTheDayData.name}
+                  </p>
+                  <p className="italic leading-normal text-sm">
+                    For {candidateOfTheDayData.position}
+                  </p>
+                </div>
+
+                <Image
+                  src={CANDIDATE_TO_IMAGE[candidateOfTheDayData.name]}
+                  alt={"SSG Logo"}
+                  width={100}
+                  height={100}
+                />
+              </>
+            </ImageDiv>
+          </section>
+
+          <section className="border rounded-lg p-4 bg-gradient-to-tr from-[#18181B] to-[#FF6969]">
+            <div className="flex items-center gap-2 text-lg font-medium mb-2">
+              <FontAwesomeIcon icon={faCalendarDays} className="text-accent" />
+              Election Timeline
+            </div>
+
+            <ElectionTimeline />
+          </section>
+
+          <section className="border rounded-lg p-4">
+            <div className="flex items-center gap-2 text-lg font-medium mb-4 text-white">
+              <FontAwesomeIcon icon={faFire} className="text-accent" />
+              Leaderboard for Engagement
+            </div>
+
+            <div className="flex gap-4 overflow-x-auto pb-2">
+              {leaderboardData.map((candidate, index) => (
+                <ImageDiv
+                  bgImage={PARTYLIST_TO_IMAGE[candidateOfTheDayData.partyList]}
+                  key={index}
+                  className="flex items-end justify-between"
+                >
+                  <>
+                    <div className="space-y-1 p-2">
+                      <p className="rounded bg-white text-accent text-xs px-2 py-1 w-fit font-semibold">
+                        {candidate.award}
+                      </p>
+                      <p className="uppercase text-white text-3xl font-bebas">
+                        {candidate.name}
+                      </p>
+                      <p className="italic text-white text-sm">
+                        For {candidate.position}
+                      </p>
+                    </div>
+
+                    <Image
+                      src={CANDIDATE_TO_IMAGE[candidate.name]}
+                      alt={`${candidate.name} image`}
+                      width={80}
+                      height={80}
+                    />
+                  </>
+                </ImageDiv>
+              ))}
+            </div>
+          </section>
         </div>
       </div>
-    </main>
+    </div>
   );
 }
--- client/app/candidates/page.tsx
+++ client/app/candidates/page.tsx
@@ -19,51 +19,71 @@ export default function CandidatesPage() {
       position: "Vice President",
       partyList: "Just",
     },
+    {
+      name: "Adrian Sajulga",
+      position: "President",
+      partyList: "United",
+    },
+    {
+      name: "Adrian Sajulga",
+      position: "Vice President",
+      partyList: "Just",
+    },
+    {
+      name: "Adrian Sajulga",
+      position: "President",
+      partyList: "United",
+    },
+    {
+      name: "Adrian Sajulga",
+      position: "Vice President",
+      partyList: "Just",
+    },
   ];
 
   return (
     <div className="p-4 space-y-6">
       <h1 className="text-3xl font-bold text-white">Candidates</h1>
 
-      {candidatesData.map((candidate, index) => {
-        const candidateImage = CANDIDATE_TO_IMAGE[candidate.name];
-        const bgImage = PARTYLIST_TO_IMAGE[candidate.partyList];
-        const partyListIcon = PARTYLIST_TO_ICON[candidate.partyList];
+      <div className="border p-4 rounded-lg space-y-2">
+        {candidatesData.map((candidate, index) => {
+          const candidateImage = CANDIDATE_TO_IMAGE[candidate.name];
+          const bgImage = PARTYLIST_TO_IMAGE[candidate.partyList];
+          const partyListIcon = PARTYLIST_TO_ICON[candidate.partyList];
 
-        return (
-          <ImageDiv
-            bgImage={bgImage}
-            key={index}
-            className="flex items-end justify-between"
-          >
-            <>
-              <Image
-                src={candidateImage}
-                alt={candidate.name}
-                width={100}
-                height={100}
-              />
+          return (
+            <ImageDiv
+              bgImage={bgImage}
+              key={index}
+              className="flex gap-4 px-4 items-center"
+            >
+              <>
+                <Image
+                  src={candidateImage}
+                  alt={candidate.name}
+                  width={100}
+                  height={100}
+                />
 
-              {/* Candidate details */}
-              <div className="text-center space-y-1">
-                <h2 className="text-2xl font-bold uppercase">
-                  {candidate.name}
-                </h2>
-                <p className="italic text-sm">{candidate.position}</p>
-                <p className="text-accent text-xs">{candidate.partyList}</p>
-              </div>
+                {/* Candidate details */}
+                <div className="space-y-1">
+                  <h1 className="text-3xl font-bold">{candidate.name}</h1>
+                  <p className="italic text-sm">For {candidate.position}</p>
+                </div>
 
-              {/* Right image */}
-              <Image
-                src={partyListIcon}
-                alt={candidate.partyList}
-                width={100}
-                height={100}
-              />
-            </>
-          </ImageDiv>
-        );
-      })}
+                {/* Right image */}
+                <Image
+                  className="ml-auto"
+                  src={partyListIcon}
+                  alt={candidate.partyList}
+                  width={100}
+                  height={100}
+                />
+              </>
+            </ImageDiv>
+          );
+        })}
+      </div>
     </div>
   );
 }
