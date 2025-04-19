284d1aa495cd224d59cadc60ac88bd708d929fe9
wetooa <derpykidyt@gmail.com>
2025-04-20 01:01:26 +0800

    Improved modularity using ImageDiv component

---
+++ client/utils/types.ts
@@ -0,0 +1,15 @@
+export type PartyList = "United" | "Just" | "Independent";
+
+export interface Candidate {
+  name: string;
+  position: string;
+  partyList: PartyList;
+}
+
+export interface CandidateWithVotes extends Candidate {
+  votes: number;
+}
+
+export interface Leaderboard extends Candidate {
+  award: string;
+}
