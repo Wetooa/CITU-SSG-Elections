0898386c7b4e057a97b7736eebedea59ee55c7e9
eloe <masterthemanok@gmail.com>
2025-04-25 07:32:43 +0800

    added documentation for candidate

--- client/app/backend/src/candidates/candidates.controller.ts
+++ client/app/backend/src/candidates/candidates.controller.ts
@@ -5,26 +5,33 @@ import { Candidate } from "src/models/candidates.model";
 @Controller("candidates")
 export class CandidatesController {
   constructor(private readonly candidatesService: CandidatesService) {}
-
+  //To create new instance of candidates, only useful when populating though
   @Post()
   async create(@Body() createCandidateDto: Candidate) {
     return await this.candidatesService.create(createCandidateDto);
   }
+
   //GET all Candidates
+  //http://localhost:8000/candidates
   @Get()
   async findAll() {
     const result = await this.candidatesService.getAllCandidates();
     return result;
   }
 
+  //Get the candidates through their balot number or name. There is a search through position pero naa na ninyo kung gamiton ninyo
   @Get("info/:query")
   async search(@Param("query") query: string) {
     if (!isNaN(Number(query))) {
+      //http://localhost:8000/candidates/info/32
       const candidate = await this.candidatesService.searchByBalotNum(+query);
       return candidate;
     } else {
+      //http://localhost:8000/candidates/info/Duterte
       const candidate = await this.candidatesService.searchByName(query);
       return candidate;
     }
+
+    // const candidate = await this.candidatesService.searchByPosition(query);
   }
 }
