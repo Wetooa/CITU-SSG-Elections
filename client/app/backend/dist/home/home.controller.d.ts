8118e2e71a152108ab534dfa4b960abd871a5270
eloe <masterthemanok@gmail.com>
2025-04-25 09:57:10 +0800

    finished home api call

--- client/app/backend/dist/home/home.controller.d.ts
+++ client/app/backend/dist/home/home.controller.d.ts
@@ -1,2 +1,8 @@
+import { HomeService } from "./home.service";
+import { Home } from "src/models/home.model";
 export declare class HomeController {
+    private readonly homeService;
+    constructor(homeService: HomeService);
+    setFeatureCandidate(featuredCandidate: Home): Promise<string>;
+    getHome(): Promise<Home[]>;
 }
--- client/app/backend/src/home/home.service.ts
+++ client/app/backend/src/home/home.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Home } from "src/models/home.model";
 
 @Injectable()
-export class HomeService {}
+export class HomeService {
+  constructor(@InjectModel("Home") private readonly HomeModel: Model<Home>) {}
+
+  async setFeaturedCandidate(featuredCandidate: Home) {
+    const newFeaturedCandidate = new this.HomeModel(featuredCandidate);
+    const result = await newFeaturedCandidate.save();
+    return result.id as string;
+  }
+
+  async getHome() {
+    const result = await this.HomeModel.find()
+      .populate("featured_candidate")
+      .exec();
+    return result as Home[];
+  }
+}
