14d8b7948483a67840abf214216990a3eaf93637
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2025-04-28 11:35:23 +0800

    cleanup

--- client/app/backend/dist/candidates/courseQuestion.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { Model } from "mongoose";
-import { CourseQuestion } from "src/models/courseQuestion.model";
-export declare class CourseQuestionService {
-    private readonly cQuestion;
-    constructor(cQuestion: Model<CourseQuestion>);
-    create(qnaDto: CourseQuestion): Promise<string>;
-    findByCandidateId(candidate_id: string): Promise<CourseQuestion>;
-    findAll(): Promise<CourseQuestion[]>;
-}
--- client/app/backend/dist/home/home.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { HomeService } from "./home.service";
-import { Home } from "src/models/home.model";
-export declare class HomeController {
-    private readonly homeService;
-    constructor(homeService: HomeService);
-    setFeatureCandidate(featuredCandidate: Home): Promise<string>;
-    getHome(): Promise<Home[]>;
-    updateFeaturedCandidate(featuredCandidate: Home): Promise<Home>;
-}
