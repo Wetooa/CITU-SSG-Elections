6d270432a04eff839b9f0f63bac629330cf26840
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2025-04-28 12:59:46 +0800

    feat: added turnout bar & responsive optimizations

--- client/components/home/sections/countdown.tsx
+++ client/components/home/sections/countdown.tsx
@@ -1,22 +1,20 @@
-import { fadeDown } from "@/utils/animations";
-import { faHourglass } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { motion } from "framer-motion";
-import { CountdownTimer } from "../countdown-timer";
+import { fadeDown } from '@/utils/animations'
+import { faHourglass } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { motion } from 'framer-motion'
+import { CountdownTimer } from '../countdown-timer'
+import { VOTING_DATE } from '@/utils/consts'
 
 export default function CountdownSection() {
-  const targetDate = new Date("2025-04-30T08:00:00");
+  const targetDate = VOTING_DATE
 
   return (
-    <motion.section
-      className="border rounded-lg bg-gradient-to-br from-[#EF4444] to-[#340A0A] p-4"
-      variants={fadeDown}
-    >
+    <motion.section className="border rounded-lg bg-gradient-to-br from-[#EF4444] to-[#340A0A] p-4" variants={fadeDown}>
       <div className="flex items-center gap-2 text-lg font-medium mb-2">
         <FontAwesomeIcon icon={faHourglass} />
         Voting starts in
       </div>
       <CountdownTimer time={targetDate} />
     </motion.section>
-  );
+  )
 }
