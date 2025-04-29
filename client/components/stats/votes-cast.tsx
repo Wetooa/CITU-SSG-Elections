cb860e2a05526c9723459a57e5350915dc199579
Nicolo Porter <98087756+nicoryne@users.noreply.github.com>
2025-04-29 22:52:44 +0800

    updated: stats page with proper responsiveness

--- client/components/stats/votes-cast.tsx
+++ client/components/stats/votes-cast.tsx
@@ -1,43 +1,40 @@
-"use client";
+'use client'
 
-import { fadeDown } from "@/utils/animations";
-import { faPrint } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { motion } from "framer-motion";
-import { VotesCastRow } from "./votes-cast-row";
-import { Candidate } from "@/utils/types";
+import { fadeDown } from '@/utils/animations'
+import { faPrint } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { motion } from 'framer-motion'
+import { VotesCastRow } from './votes-cast-row'
+import type { Candidate } from '@/utils/types'
 
 interface VotesCastSectionProps {
-  data: Record<string, Candidate[]>;
+  data: Record<string, Candidate[]>
 }
 
 export const VotesCastSection = ({ data }: VotesCastSectionProps) => {
   return (
     <>
-      <motion.section
-        className="relative border rounded-xl p-2 sm:p-4 overflow-hidden"
-        variants={fadeDown}
-      >
+      <motion.section className="relative border rounded-xl p-2 sm:p-4 overflow-hidden" variants={fadeDown}>
         {/* TODO: port hardcoded values to globals or smthn*/}
         <div className="absolute inset-0 bg-gradient-to-bl from-[#FF6969] to-[#18181B] opacity-10 pointer-events-none" />
 
         <div className="relative z-10 h-full w-full">
-          <div className="flex items-center gap-2 mb-2">
+          <div className="flex items-center justify-center sm:justify-start gap-2 mb-4">
             <FontAwesomeIcon icon={faPrint} className="text-accent" />
             <h2 className="text-lg font-medium">Votes Cast</h2>
           </div>
-          <div className="flex flex-col gap-2">
-            {Object.keys(data).map((position, index) => (
-              <VotesCastRow
-                key={index}
-                position={position}
-                candidates={data[position]}
-                index={index}
-              />
-            ))}
-          </div>
+
+          {Object.keys(data).length > 0 ? (
+            <div className="flex flex-col gap-4">
+              {Object.keys(data).map((position, index) => (
+                <VotesCastRow key={index} position={position} candidates={data[position]} index={index} />
+              ))}
+            </div>
+          ) : (
+            <div className="flex justify-center items-center h-40 text-gray-400">No voting data available yet</div>
+          )}
         </div>
       </motion.section>
     </>
-  );
-};
+  )
+}
